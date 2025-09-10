3d6c05d0bff085d5c10d1a0bdc61ed7a6029c4a7
Elianor <Elianor@lab>
2025-09-10 14:20:40 +0300

    turtle location

--- Program1Turtle.cs
+++ Program1Turtle.cs
@@ -1,43 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Versioning;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Unit4.TurtleLib;
 
 namespace Turtle22
 {
-    internal partial class Program
+    partial class Program
     {
+
+        static Random rnd = new Random();
         /// <summary>
         /// Turtle program
         /// </summary>
         static void Main1()
         {
+
+            double x = 0;
+            double y = 0;
             Turtle t1 = new Turtle();
 
             //t1.MoveBackward(200);
-            t1.SetDelay(2);
+            t1.SetDelay(10);
             t1.TailUp();
 
 
             t1.MoveForward(180);
             t1.TurnLeft(90);
-            t1.MoveBackward(135);
+            t1.MoveBackward(180);
             t1.TailDown();
+
+
             t1.MoveForward(360);
+
+            //x = Math.Round(t1.GetXLocation(), 2);
+            //y = Math.Round(t1.GetYLocation(), 2);
+            //Console.WriteLine($"x1: {x}, y: {y}  ");
+
             t1.TurnLeft(90);
             t1.MoveForward(360);
+
+            //x = Math.Round(t1.GetXLocation(), 2);
+            //y = Math.Round(t1.GetYLocation(), 2);
+            //Console.WriteLine($"x1: {x}, y: {y}  ");
+
+
             t1.TurnLeft(90);
             t1.MoveForward(360);
+
+            //x = Math.Round(t1.GetXLocation(), 2);
+            //y = Math.Round(t1.GetYLocation(), 2);
+            //Console.WriteLine($"x1: {x}, y: {y}  ");
+           
+            
             t1.TurnLeft(90);
             t1.MoveForward(360);
+
+            //x = Math.Round(t1.GetXLocation(), 2);
+            //y = Math.Round(t1.GetYLocation(), 2);
+            //Console.WriteLine($"x1: {x}, y: {y}  ");
+           
+            
             t1.TailUp();
-            t1.MoveBackward(255);
+            t1.MoveBackward(261);
             t1.TurnLeft(90);
             t1.MoveForward(20);
             t1.TurnLeft(60.5);
             t1.TailDown();
+
+            //x =  Math.Round (t1.GetXLocation(),2);
+            //y =  Math.Round (t1.GetYLocation(),2);
+            //Console.WriteLine($"x1: {x}, y: {y}  "  );
 
             double step = 100 * 2 * Math.PI / 36;
             for (int i = 0; i < 73; i++)
@@ -47,6 +83,18 @@ namespace Turtle22
                 t1.TurnRight(5);
 
             }
+
+            double x1, y1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                x1 = rnd.NextDouble() * 360 - 180;
+                y1 = rnd.NextDouble() * 360 - 180;
+
+                Console.WriteLine($"x1: {x1}, y: {y1}  ");
+            }
+
+
 
 
 
